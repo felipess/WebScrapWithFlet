c10a9fb52ac861cbb5f42e6c7f67b06b71be581b
Felipe Soares <felipediogenes87@gmail.com>
2024-07-20 23:50:47 -0300

    botão iniciar e consulta navegando ao site

--- test_flet.py
+++ test_flet.py
@@ -1,7 +1,132 @@
 import flet as ft
 from VarasFederais import VarasFederais
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
+import threading
+
+# Definição dos eventos globais
+running_event = threading.Event()
+termino_event = threading.Event()
+
+def executar_consulta():
+    running_event.set()
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')  # Desativado o modo headless para observação
+    options.add_argument("--blink-settings=loadMediaAutomatically=2")  # Configuração adicional
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=options)
+    wait = WebDriverWait(driver, 20)  # Aumentando o tempo de espera para 20 segundos
+
+    try:
+        resultados = []
+        driver.get("https://eproc.jfpr.jus.br/eprocV2/externo_controlador.php?acao=pauta_audiencias")
+        wait.until(EC.presence_of_element_located((By.ID, "divRowVaraFederal")))
+        campo_data_inicio = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataInicio")))
+
+        # Atualize as datas usando os valores atuais dos campos de entrada
+        data_inicio = entry_data_inicio.value.strip()
+        campo_data_inicio.clear()
+        campo_data_inicio.send_keys(data_inicio)           
+        
+        data_fim = entry_data_fim.value.strip()
+        campo_data_fim = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataTermino")))
+        campo_data_fim.clear()
+        campo_data_fim.send_keys(data_fim)
+
+        titulos = [" Data/Hora: ", " Autos:", "", "", "", "Status", "Sistema"]
+        
+        for idx, vara in enumerate(varas_federais):
+            if termino_event.is_set():  # Verifica se o evento de término foi sinalizado
+                break
+            
+            try:
+                # Atualiza o spinner_label com a vara atual
+                spinner_label.value = f"Consultando {vara}..."
+                page.update()
+
+                vara_federal_div = wait.until(EC.presence_of_element_located((By.ID, "divRowVaraFederal")))
+                dropdown_button = vara_federal_div.find_element(By.CLASS_NAME, "dropdown-toggle")
+                dropdown_button.click()
+                vara_federal_option = wait.until(EC.element_to_be_clickable((By.XPATH, f"//option[text()='{vara}']")))
+                vara_federal_option.click()
+
+                # Adiciona um pequeno intervalo de espera entre as mudanças de vara
+                if idx > 0:
+                    time.sleep(2)  # Ajuste o tempo conforme necessário
+
+                # Clique no botão "Executar Consulta"
+                botao_consultar = wait.until(EC.element_to_be_clickable((By.ID, "btnConsultar")))
+                botao_consultar.click()
+
+                # Verifica se há resultados na tabela ou se encontrou a mensagem "Nenhum resultado encontrado."
+                div_infra_area_tabela = wait.until(EC.presence_of_element_located((By.ID, "divInfraAreaTabela")))
+                tabela_text = div_infra_area_tabela.text
+
+                if "Nenhum resultado encontrado." in tabela_text:
+                    resultados.append("")
+                else:
+                    resultados.append(f"Resultados da {vara}\n")
+                    
+                    tabela = driver.find_element(By.ID, "tblAudienciasEproc")
+                    linhas = tabela.find_elements(By.TAG_NAME, "tr")
+                    encontrou = False
+                    termos = ["custódia", "custodia"]
+
+                    for linha in linhas:
+                        texto_normalizado = linha.text.lower()
+                        if any(termo in texto_normalizado for termo in termos):
+                            conteudo_linha = ""
+                            tds = linha.find_elements(By.TAG_NAME, "td")
+                            for titulo, td in zip(titulos, tds):
+                                if titulo == "Sistema" or titulo == "Status":
+                                    continue
+                                td_html = td.get_attribute('innerHTML')
+                                td_soup = BeautifulSoup(td_html, 'html.parser')
+                                td_text = td_soup.get_text(separator=" ").split("Classe:")[0].strip()
+                                conteudo_linha += f"{titulo} {td_text}\n"
+                            resultados.append(conteudo_linha)
+                            resultados.append("\n")
+                            encontrou = True
+
+                    if not encontrou:
+                        resultados.pop()
+                        resultados.append("")
+
+            except Exception as e:
+                resultados.append(f"Ocorreu um erro na consulta da vara {vara}.\n")
+
+        # Atualize a interface do Flet com os resultados
+        atualizar_resultados(resultados)
+
+    except Exception as e:
+        resultados.append("Ocorreu um erro durante a consulta. Tente novamente mais tarde.")
+        
+    finally:
+        spinner_label.value = ""  # Limpa o spinner após a consulta
+        page.update()
+        running_event.clear()  # Marca a consulta como finalizada
+        driver.quit()
+        if not termino_event.is_set():
+            agendar_proxima_consulta()
+
+# Função para atualizar o resultado na interface do Flet
+def atualizar_resultados(resultados):
+    result_text = "\n".join(resultados)
+    text_area.value = result_text
+    page.update()
+
+def iniciar_consulta(e):
+    threading.Thread(target=executar_consulta).start()
 
 def main(page: ft.Page):
+    global varas_federais, varas_dropdown, selected_varas_list, text_area, spinner_label, entry_data_inicio, entry_data_fim
+    
     # Define o tamanho inicial da janela
     page.window_width = 640
     page.window_height = 900
@@ -65,17 +190,23 @@ def main(page: ft.Page):
     )
 
     # Área de texto para exibir resultados
-    text_area_content = (
-        "Aqui você pode adicionar um texto longo que será exibido sem rolagem. " * 10
-    )
-
+    text_area_content = "Aqui você pode adicionar um texto longo que será exibido sem rolagem. " * 10
     text_area = ft.Text(
         value=text_area_content,
         size=12,
         text_align=ft.TextAlign.LEFT,
-        width=580,  # Ajuste a largura conforme necessário
-        height=250, # Ajuste a altura conforme necessário
-        #bgcolor=ft.colors.BLUE_GREY_900  # Cor de fundo para visualização
+        width=580,
+        height=250,
+        bgcolor=ft.colors.BLACK,  # Cor de fundo para visualização
+    )
+
+    spinner_label = ft.Text("", size=12, text_align=ft.TextAlign.CENTER)
+    entry_data_inicio = ft.TextField(label="Data Início", width=580, height=40)
+    entry_data_fim = ft.TextField(label="Data Fim", width=580, height=40)
+    
+    consulta_button = ft.ElevatedButton(
+        text="Iniciar Consulta",
+        on_click=iniciar_consulta
     )
 
     page.add(
@@ -86,6 +217,10 @@ def main(page: ft.Page):
                 add_button,
                 ft.Text("Varas Selecionadas", size=12),
                 selected_varas_list,
+                spinner_label,
+                entry_data_inicio,
+                entry_data_fim,
+                consulta_button,
                 ft.Container(
                     content=text_area,
                     margin=10,
@@ -99,7 +234,7 @@ def main(page: ft.Page):
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER
-        ),            
+        ),
     )
 
 ft.app(target=main)
