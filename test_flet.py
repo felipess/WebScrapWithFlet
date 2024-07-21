bf850408f33d8317bf030ddb71c3c9ee25f82df7
Felipe Soares <felipediogenes87@gmail.com>
2024-07-21 13:00:49 -0300

    Consulta funcionando

--- test_flet.py
+++ test_flet.py
@@ -19,30 +19,32 @@ text_area = None
 spinner_label = None
 page = None
 varas_federais = []
+varas_selecionadas = []
+
+entry_data_inicio = ft.TextField(label="Data Início", value="15/07/2024")
+entry_data_fim = ft.TextField(label="Data Fim", value="19/07/2024")
 
 def atualizar_resultados(resultados):
     global text_area, page
-    if text_area and page:
+    if text_area and text_area.content and page:
         result_text = "\n".join(resultados)
-        text_area.value = result_text
+        text_area.content.value = result_text
         page.update()
 
-# Definir uma função para estimar a largura do texto
 def get_text_width(text, font_size):
-    # Ajuste esse valor conforme necessário para a precisão desejada
-    average_char_width = 7  # Largura média de um caractere em pixels
+    average_char_width = 7
     return len(text) * average_char_width
 
 def agendar_proxima_consulta():
-    next_run = datetime.datetime.now() + datetime.timedelta(minutes=30)  # Exemplo: Próxima consulta em 30 minutos
+    next_run = datetime.datetime.now() + datetime.timedelta(minutes=30)
     delay = (next_run - datetime.datetime.now()).total_seconds()
     threading.Timer(delay, lambda: executar_consulta(page)).start()
 
-def executar_consulta(page):
-    global driver
+def executar_consulta(pg):
+    global driver, page
+    page = pg
     running_event.set()
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # Rodar em modo headless se necessário
     options.add_argument("--blink-settings=loadMediaAutomatically=2")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
@@ -55,21 +57,21 @@ def executar_consulta(page):
         driver.get("https://eproc.jfpr.jus.br/eprocV2/externo_controlador.php?acao=pauta_audiencias")
         wait.until(EC.presence_of_element_located((By.ID, "divRowVaraFederal")))
 
-        campo_data_inicio = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataInicio")))
-        campo_data_fim = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataTermino")))
-
-        # Atualize as datas usando os valores atuais dos campos de entrada
         data_inicio = entry_data_inicio.value.strip()
+        data_fim = entry_data_fim.value.strip()
+
+        campo_data_inicio = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataInicio")))
         campo_data_inicio.clear()
         campo_data_inicio.send_keys(data_inicio)
 
-        data_fim = entry_data_fim.value.strip()
+        campo_data_fim = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataTermino")))
         campo_data_fim.clear()
         campo_data_fim.send_keys(data_fim)
 
         titulos = [" Data/Hora: ", " Autos:", "", "", "", "Status", "Sistema"]
 
-        for idx, vara in enumerate(varas_federais):
+        for idx, vara in enumerate(varas_selecionadas):
+            print(vara)
             if termino_event.is_set():
                 break
 
@@ -80,27 +82,17 @@ def executar_consulta(page):
                     page.update()
 
                 vara_federal_div = wait.until(EC.presence_of_element_located((By.ID, "divRowVaraFederal")))
-
-                # Clique no dropdown para selecionar a Vara
                 dropdown_button = vara_federal_div.find_element(By.CLASS_NAME, "dropdown-toggle")
                 dropdown_button.click()
+                vara_federal_option = wait.until(EC.element_to_be_clickable((By.XPATH, f"//option[text()='{vara}']")))
+                vara_federal_option.click()
 
-                # Aguarde e selecione a opção desejada
-                varas_opcoes = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//select[@id='ddlVaraFederal']/option")))
-                for opcao in varas_opcoes:
-                    if opcao.text == vara:
-                        opcao.click()
-                        break
-
-                # Adiciona um pequeno intervalo de espera entre as mudanças de vara
                 if idx > 0:
                     time.sleep(2)
 
-                # Clique no botão "Executar Consulta"
                 botao_consultar = wait.until(EC.element_to_be_clickable((By.ID, "btnConsultar")))
                 botao_consultar.click()
 
-                # Verifica se há resultados na tabela ou se encontrou a mensagem "Nenhum resultado encontrado."
                 div_infra_area_tabela = wait.until(EC.presence_of_element_located((By.ID, "divInfraAreaTabela")))
                 tabela_text = div_infra_area_tabela.text
 
@@ -137,43 +129,39 @@ def executar_consulta(page):
             except Exception as e:
                 resultados.append(f"Ocorreu um erro na consulta da vara {vara}.\n")
 
-        # Atualize a interface do Flet com os resultados
-        atualizar_resultados(resultados)
+            atualizar_resultados(resultados)
 
     except Exception as e:
         resultados.append("Ocorreu um erro durante a consulta. Tente novamente mais tarde.")
         
     finally:
         if spinner_label:
-            spinner_label.value = ""  # Limpa o spinner após a consulta
+            spinner_label.value = ""
             page.update()
-        running_event.clear()  # Marca a consulta como finalizada
+        running_event.clear()
         if driver:
-            driver.quit()  # Garante que o driver seja fechado corretamente
+            driver.quit()
         if not termino_event.is_set():
             agendar_proxima_consulta()
 
-# Adiciona um botão para iniciar a consulta manualmente
 start_button = ft.ElevatedButton(
     text="Iniciar Consulta",
     on_click=lambda e: executar_consulta(page)
 )
 
-def main(page: ft.Page):
-    global entry_data_inicio, entry_data_fim, spinner_label, text_area, varas_federais
+def main(pg: ft.Page):
+    global entry_data_inicio, entry_data_fim, spinner_label, text_area, varas_federais, varas_selecionadas, page
 
-    # Define o tamanho inicial da janela
+    page = pg
+
     page.window.width = 640
     page.window.height = 900
-
     page.title = "Containers - clickable and not"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    # Lista de varas federais do Enum
     varas_federais = [vara.value for vara in VarasFederais]
 
-    # Lista de varas selecionadas por padrão
     varas_selecionadas_iniciais = [
         VarasFederais.VARA_GUAIRA.value,
         VarasFederais.VARA_FOZ_3.value,
@@ -189,7 +177,6 @@ def main(page: ft.Page):
         VarasFederais.VARA_CURITIBA_23.value,
     ]
 
-    # Inicializa com as varas selecionadas por padrão
     varas_selecionadas = varas_selecionadas_iniciais.copy()
 
     def add_varas(e):
@@ -206,14 +193,13 @@ def main(page: ft.Page):
     
 
     def update_varas_selecionadas():
-        # Garante que `selected_varas_list` seja um Container ou algo que possa conter uma lista de controles
         selected_varas_list.controls = [
             ft.Container(
                 content=ft.Row(
                     controls=[
                         ft.Container(
                             content=ft.Text(varas, size=10),
-                            width=get_text_width(varas, 10),  # Ajusta a largura do container com base no tamanho do texto + margem
+                            width=get_text_width(varas, 10),
                             height=25,
                             bgcolor=ft.colors.BLUE,
                             padding=0,
@@ -230,13 +216,8 @@ def main(page: ft.Page):
                             tooltip="Remover",
                         )
                     ],
-                    alignment=ft.MainAxisAlignment.START,  # Alinha os itens na horizontal
-                    #spacing=10  # Adiciona espaçamento entre os itens
+                    alignment=ft.MainAxisAlignment.START,
                 ),
-                # padding=5,
-                # margin=5,
-                # border_radius=5,
-                # bgcolor=ft.colors.WHITE
             )
             for varas in varas_selecionadas
         ]
@@ -247,28 +228,27 @@ def main(page: ft.Page):
         varas_dropdown.options = [ft.dropdown.Option(vara) for vara in available_options]
         page.update()
 
-    # Inicializa o Dropdown com todas as varas disponíveis, exceto as já selecionadas
     available_varas = [vara for vara in varas_federais if vara not in varas_selecionadas]
     varas_dropdown = ft.Dropdown(
         options=[ft.dropdown.Option(vara) for vara in available_varas],
-        value=None,  # Nenhum valor padrão selecionado no Dropdown
+        value=None,
         width=580,
-        text_style=ft.TextStyle(size=10),  # Ajusta o tamanho da fonte no Dropdown
-        height=40  # Ajusta a altura do Dropdown, o que pode ajudar a ajustar o espaçamento visual
+        text_style=ft.TextStyle(size=10),
+        height=40
     )
 
     add_button = ft.ElevatedButton(
-        text="Adicionar",
-        on_click=add_varas
+        text="Adicionar Vara",
+        on_click=add_varas,
     )
 
     selected_varas_list = ft.Column(
         controls=[
             ft.Row(
-                [
+                controls=[
                     ft.Container(
                         content=ft.Text(varas, size=10),
-                        width=get_text_width(varas, 10),  # Ajusta a largura do container com base no tamanho do texto + margem
+                        width=get_text_width(varas, 10),
                         height=25,
                         bgcolor=ft.colors.BLUE,
                         padding=0,
@@ -285,58 +265,34 @@ def main(page: ft.Page):
                         tooltip="Remover",
                     )
                 ],
-                alignment=ft.MainAxisAlignment.START,  # Alinha os itens na horizontal
-            ) for varas in varas_selecionadas
+                alignment=ft.MainAxisAlignment.START,
+            )
+            for varas in varas_selecionadas
         ]
     )
 
-    # Área de texto para exibir resultados (substituindo TextArea por Text)
-    text_area_content = (
-        "Aqui você pode adicionar um texto longo que será exibido sem rolagem. " * 10
-    )
+    entry_data_inicio = ft.TextField(label="Data Início", value="15/07/2024", width=150)
+    entry_data_fim = ft.TextField(label="Data Fim", value="19/07/2024", width=150)
+    spinner_label = ft.Text("")
 
-    text_area = ft.Container(
-        content=ft.Text(
-            value=text_area_content,
-            size=12,
-            text_align=ft.TextAlign.LEFT
-        ),
-        width=580,
-        height=250,
-        bgcolor=ft.colors.BLACK,
-        padding=10,
-        margin=10
-    )
+    text_area = ft.Container(content=ft.Text("Resultados..."))
 
-    spinner_label = ft.Text("", size=12)
+    update_varas_selecionadas()
 
     page.add(
         ft.Column(
-            controls=[
-                ft.Text("Selecione a Vara", size=12),
-                varas_dropdown,
-                add_button,
-                ft.Text("Varas Selecionadas", size=12),
+            [
+                ft.Row([entry_data_inicio, entry_data_fim]),
+                ft.Row([varas_dropdown, add_button]),
                 selected_varas_list,
+                ft.Row([start_button]),
                 spinner_label,
                 text_area,
-                start_button  # Adiciona o botão para iniciar a consulta manualmente
             ],
+            spacing=10,
             alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER
-        ),            
+        )
     )
 
-    # Inicia a execução da consulta quando a aplicação inicia
-    # executar_consulta(page)
-
-    # Add the event handler to close the application
-    def close_app(e):
-        global driver
-        if driver:
-            driver.quit()
-        page.window.close()
-
-    page.on_close = close_app
-
-ft.app(target=main)
+if __name__ == "__main__":
+    ft.app(target=main)
