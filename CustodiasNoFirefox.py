29012062dcd51eefa975ea144ac39b918163c28b
Felipe Soares <felipediogenes87@gmail.com>
2024-10-11 18:50:37 -0300

    V3.0 funcionando

--- CustodiasNoFirefox.py
+++ CustodiasNoFirefox.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
-from VarasFederais import VarasFederais
 from bs4 import BeautifulSoup
 import datetime
 import pyperclip
@@ -17,7 +16,7 @@ import psutil
 data_validade = datetime.datetime(2024, 12, 8)  # Defina sua data de validade aqui
 
 # Variáveis globais
-VERSION = "2.0"
+VERSION = "3.0"
 driver = None
 driver_pid = None
 running_event = threading.Event()
@@ -35,8 +34,8 @@ sizeFontRows = 10
 
 # Definição das datas padrão
 hoje = datetime.datetime.now()
-data_inicio_default = hoje.strftime("%d/%m/%Y")
-data_fim_default = (hoje + datetime.timedelta(days=1)).strftime("%d/%m/%Y")
+data_inicio_default = hoje.strftime("%d/%m/%Y")  # Data de hoje
+data_fim_default = (hoje + datetime.timedelta(days=1)).strftime("%d/%m/%Y")  # Data de amanhã
 
 # Variável global para armazenar a mensagem de nenhum resultado
 mensagem_nenhum_resultado = None
@@ -71,37 +70,26 @@ def verificar_validade():
         return False
     return True
 
-# Inicialização dos labels com datas e horas atuais
 def get_formatted_datetime():
     now = datetime.datetime.now()
-    #return now.strftime("%d/%m/%Y %H:%M:%S")
     return now.strftime("%H:%M:%S")
 
 def atualizar_rodape():
     global page
-    # global label_dev
 
 def copiar_linha(conteudo_linha):
     """Função para copiar o conteúdo da linha para a área de transferência."""
-    conteudo_ordenado = [conteudo_linha[i] for i in ordem_colunas]  # Organiza o conteúdo conforme a ordem definida
+    conteudo_ordenado = [conteudo_linha[i] for i in ordem_colunas]
 
-    # Modifica o conteúdo da coluna 0
     if len(conteudo_ordenado) > 0:  # Verifica se a coluna 0 existe
         coluna_0_texto = conteudo_ordenado[0]
 
-        # Remove "Observação:" e tudo o que vem depois
         if "Observação:" in coluna_0_texto:
-            coluna_0_texto = coluna_0_texto.split("Observação:")[0].strip()  # Corta até antes de "Observação:"
-        
-        conteudo_ordenado[0] = coluna_0_texto  # Atualiza a coluna 0
+            coluna_0_texto = coluna_0_texto.split("Observação:")[0].strip() # Remove "Observação:" e tudo o que vem depois       
+        conteudo_ordenado[0] = coluna_0_texto  
 
-    # Modifica o conteúdo da coluna 4
     if len(conteudo_ordenado) > 4:  # Verifica se a coluna 4 existe
         coluna_4_texto = conteudo_ordenado[4]
-
-        # Remove tudo após "Observação:" e a própria palavra
-        if "Observação:" in coluna_4_texto:
-            coluna_4_texto = coluna_4_texto.split("Observação:")[0].strip()  # Corta até antes de "Observação:"
         
         conteudo_ordenado[4] = coluna_4_texto  # Atualiza a coluna 4 com o texto modificado
 
@@ -311,7 +299,7 @@ def initialize_webdriver():
     global driver_pid
     """Initialize the Selenium WebDriver with necessary options."""
     options = Options()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--width=1080")  # Define a largura
@@ -349,7 +337,6 @@ def windowSize(page):
 
 def executar_consulta(page):
     global driver, driver_pid, executado, mensagem_nenhum_resultado, resultados_anteriores
-    
     print("Consulta Iniciada...")
 
     driver = initialize_webdriver()
@@ -369,100 +356,94 @@ def executar_consulta(page):
         driver.get("https://eproc.jfpr.jus.br/eprocV2/externo_controlador.php?acao=pauta_audiencias")
 
         time.sleep(1)
-
         
         wait = WebDriverWait(driver, 30)
-        wait.until(EC.presence_of_element_located((By.ID, "divRowVaraFederal")))
+        consultar_por = wait.until(EC.presence_of_element_located((By.ID, "divColConsultarPor")))
+        time.sleep(1)
+
+        dropdown_button = consultar_por.find_element(By.CLASS_NAME, "dropdown-toggle")
+        dropdown_button.click()
+        time.sleep(1)
+
+        # Aguarde a lista de opções ser exibida
+        options = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".dropdown-menu .dropdown-item")))
+
+        for option in options:
+            if "Intervalo de Data e Competência" in option.text:
+                option.click()
+                print("Consultando por Intervalo de Data e Competência")
+                break
+
+        time.sleep(1)
 
         data_inicio = entry_data_inicio.value.strip()
         data_fim = entry_data_fim.value.strip()
 
-        campo_data_inicio = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataInicio")))
+        print(f"Data de início: {data_inicio}, Data de fim: {data_fim}")
+
+        campo_data_inicio = wait.until(EC.presence_of_element_located((By.ID, "txtDataInicio")))
+        driver.execute_script("arguments[0].scrollIntoView(true);", campo_data_inicio)
+        time.sleep(1)  # Adicione um pequeno atraso, se necessário
         campo_data_inicio.clear()
         campo_data_inicio.send_keys(data_inicio)
 
-        campo_data_fim = wait.until(EC.presence_of_element_located((By.ID, "txtVFDataTermino")))
+        time.sleep(1)
+
+        campo_data_fim = wait.until(EC.presence_of_element_located((By.ID, "txtDataTermino")))
         campo_data_fim.clear()
         campo_data_fim.send_keys(data_fim)
 
-        for idx, vara in enumerate(varas_selecionadas):
-            print(f"Consultando: {vara}")  # Certifique-se que o valor está correto
+        time.sleep(1)
+        
+        botao_consultar = wait.until(EC.element_to_be_clickable((By.ID, "btnConsultar")))
+        botao_consultar.click()
 
-            if termino_event.is_set():
-                break
+        time.sleep(2)
 
-            try:
-                spinner_label.value = f"Consultando {vara}..."
-                page.update()
 
-                vara_federal_div = wait.until(EC.presence_of_element_located((By.ID, "divRowVaraFederal")))
-                dropdown_button = vara_federal_div.find_element(By.CLASS_NAME, "dropdown-toggle")
-                dropdown_button.click()
+        # Verificar se há a mensagem de "Nenhum resultado encontrado"
+        mensagem_erro = wait.until(EC.presence_of_element_located((By.ID, "divInfraAreaTabela")))
+        if "Nenhum resultado encontrado" in mensagem_erro.text:
+            print(f"Nenhum resultado encontrado.")
+            
 
-                # Captura o campo de entrada do dropdown
-                search_input = vara_federal_div.find_element(By.CSS_SELECTOR, "input.form-control")  # Ajuste o seletor se necessário
+        # Esperar até que a tabela esteja presente
+        tabela = wait.until(EC.presence_of_element_located((By.ID, "tblAudienciasEproc")))
+        linhas = tabela.find_elements(By.TAG_NAME, "tr")
 
-                # Foca no campo de entrada
-                search_input.click()  # Clica para garantir que está ativo
-
-                # Digita o nome da vara
-                search_input.send_keys(vara)
-
-                # Pressiona Enter para selecionar a primeira opção filtrada
-                search_input.send_keys(Keys.RETURN)
-
-                # Botão de consultar
-                botao_consultar = wait.until(EC.element_to_be_clickable((By.ID, "btnConsultar")))
-                botao_consultar.click()
-
-                # Verificar se há a mensagem de "Nenhum resultado encontrado"
-                mensagem_erro = wait.until(EC.presence_of_element_located((By.ID, "divInfraAreaTabela")))
-                if "Nenhum resultado encontrado" in mensagem_erro.text:
-                    print(f"Nenhum resultado encontrado para {vara}")
-                    continue
-
-                # Esperar até que a tabela esteja presente
-                tabela = wait.until(EC.presence_of_element_located((By.ID, "tblAudienciasEproc")))
-                linhas = tabela.find_elements(By.TAG_NAME, "tr")
-
-                for linha in linhas:
-                    texto_normalizado = linha.text.lower()
-                    if any(termo in texto_normalizado for termo in ["custódia", "custodia"]):
-                        tds = linha.find_elements(By.TAG_NAME, "td")
-                        conteudo_linha = []
-                        erro_encontrado = False
-                        for td in tds:
-                            td_html = td.get_attribute('innerHTML')
-                            td_soup = BeautifulSoup(td_html, 'html.parser')
-                            td_text = td_soup.get_text(separator=" ").split("Classe:")[0].strip()  # Modificação aqui
-                            if "ocorreu um erro" in td_text.lower():
-                                erro_encontrado = True
-                                break
-                            conteudo_linha.append(td_text)
-                        if not erro_encontrado and len(conteudo_linha) == len(titulos):
-                            resultados.append(conteudo_linha)
-
-            except Exception as e:
-                print(f"Erro ao consultar a vara {vara}: {e}")
-                return
+        for linha in linhas:
+            texto_normalizado = linha.text.lower()
+            if any(termo in texto_normalizado for termo in ["custódia", "custodia"]):
+                tds = linha.find_elements(By.TAG_NAME, "td")
+                conteudo_linha = []
+                erro_encontrado = False
+                for td in tds:
+                    td_html = td.get_attribute('innerHTML')
+                    td_soup = BeautifulSoup(td_html, 'html.parser')
+                    td_text = td_soup.get_text(separator=" ").split("Classe:")[0].strip()  
+                    if "ocorreu um erro" in td_text.lower():
+                        erro_encontrado = True
+                        break
+                    conteudo_linha.append(td_text)
+                if not erro_encontrado and len(conteudo_linha) == len(titulos):
+                    resultados.append(conteudo_linha)
 
         if not resultados:
             resultados = []
             mensagem_nenhum_resultado = "Nenhum resultado encontrado."
 
         atualizar_resultados(resultados)
-        atualizar_rodape()  # Atualiza a nota de rodapé        
+        atualizar_rodape()       
 
     except Exception as e:
         print(f"Erro geral: {e}")
         resultados.append([""] * len(titulos))  # Adiciona uma linha vazia se houver um erro geral
         atualizar_resultados(resultados)
-        atualizar_rodape()  # Atualiza a nota de rodapé
+        atualizar_rodape()  
         return
 
         
     finally:        
-        # Atualiza o estado do botão e outros elementos da interface
         start_button.disabled = True
         start_button.update()
 
@@ -471,12 +452,11 @@ def executar_consulta(page):
             executado = True
             page.update()
         
-         # Tenta encerrar o driver corretamente
         if driver:
             driver.quit()
 
         if driver_pid:
-            finalizar_driver()  # Chama a função para encerrar o processo
+            finalizar_driver()  
         
         running_event.clear()
         if not termino_event.is_set():
@@ -484,11 +464,10 @@ def executar_consulta(page):
         print("Consulta finalizada")
 
 def main(pg: ft.Page):
-    global driver, driver_pid, entry_data_inicio, entry_data_fim, spinner_label, varas_federais, varas_selecionadas, page, ultima_consulta, proxima_consulta 
-    # selected_varas_list
+    global driver, driver_pid, entry_data_inicio, entry_data_fim, spinner_label, page, ultima_consulta, proxima_consulta 
     # text_area
     page = pg
-    page.on_close = on_close  # Define o manipulador para o fechamento da janela
+    page.on_close = on_close  
     windowSize(page)
 
     page.title = f"Pesquisa automatizada - Circunscrições da JF do Paraná - Versão {VERSION}"
@@ -499,123 +478,12 @@ def main(pg: ft.Page):
     if not verificar_validade():
         pg.add(ft.Text("Este programa não é mais válido - permissão expirada. Entre em contato com o desenvolvedor feliped@mpf.mp.br pelo Zoom."))
         pg.update()
-        return  # Encerra a execução se a validade não for atendida
-
-    varas_federais = [vara.value for vara in VarasFederais]
-
-    # Mapeamento dos valores para labels personalizados
-    varas_labels = {
-        VarasFederais.VARA_GUAIRA.value: "1ª VF Guaira",
-        VarasFederais.VARA_FOZ_3.value: "3 VF de Foz",
-        VarasFederais.VARA_UMUARAMA_1.value: "1ª VF Umuarama",
-        VarasFederais.VARA_PONTA_GROSSA_1.value: "1ª VF Ponta Grossa",
-        VarasFederais.VARA_MARINGA_3.value: "3ª VF Maringá",
-        #VarasFederais.VARA_MARINGA_6.value: "6ª VF Maringá", #Somente previdenciarios
-        VarasFederais.VARA_CASCAVEL_4.value: "4ª VF Cascavel",
-        VarasFederais.VARA_FOZ_5.value: "5ª VF Foz",
-        VarasFederais.VARA_LONDRINA_5.value: "5ª VF Londrina",
-        VarasFederais.VARA_CURITIBA_9.value: "9ª VF Curitiba",
-        VarasFederais.VARA_CURITIBA_13.value: "13ª VF Curitiba",
-        VarasFederais.VARA_CURITIBA_14.value: "14ª VF Curitiba",
-        VarasFederais.VARA_CURITIBA_23.value: "23ª VF Curitiba",
-    }
-
-    varas_selecionadas_iniciais = [
-        VarasFederais.VARA_GUAIRA.value, #
-        VarasFederais.VARA_FOZ_3.value, #
-        VarasFederais.VARA_UMUARAMA_1.value, #
-        VarasFederais.VARA_PONTA_GROSSA_1.value, #
-        VarasFederais.VARA_MARINGA_3.value, #
-        #VarasFederais.VARA_MARINGA_6.value, #Somente previdenciarios
-        VarasFederais.VARA_CASCAVEL_4.value, #
-        VarasFederais.VARA_FOZ_5.value, #
-        VarasFederais.VARA_LONDRINA_5.value, #
-        VarasFederais.VARA_CURITIBA_9.value, #
-        VarasFederais.VARA_CURITIBA_13.value, #
-        VarasFederais.VARA_CURITIBA_14.value, #
-        VarasFederais.VARA_CURITIBA_23.value, #
-    ]
-
-    varas_selecionadas = varas_selecionadas_iniciais.copy()    
-
-    def update_varas_selecionadas():
-        varas_items = [
-            ft.Container(
-                content=ft.ResponsiveRow(
-                    controls=[
-                        ft.Container(
-                            content=ft.Text(varas_labels.get(varas, varas), size=10),  # Usa o label personalizado se disponível
-                            padding=ft.Padding(left=20, top=2, right=0, bottom=0),  # Espaço à esquerda do texto
-                            width=get_text_width(varas_labels.get(varas, varas), 10) + 40,  # Ajustar largura para incluir espaço
-                            height=25,
-                            bgcolor=ft.colors.BLUE,
-                            border_radius=5,
-                            #on_click=lambda e, v=varas: remove_varas(v),
-                            tooltip="Remover",
-                        ),
-                    ],
-                    spacing=5,
-                    alignment=ft.MainAxisAlignment.START,
-                ),
-                padding=5,
-                col={"xs": 6, "sm": 3, "md": 3, "lg": 2, "xl": 2, "xxl": 1},
-            )
-            for varas in varas_selecionadas
-        ]
-
-        selected_varas_list.controls = [
-            ft.Text(
-                "Varas Federais selecionadas",
-                size=16,
-                weight=ft.FontWeight.BOLD,
-            ),
-            ft.Container(
-                content=ft.ResponsiveRow(
-                    controls=varas_items,
-                    spacing=10,
-                ),
-                padding=10,
-                ink=True,
-                border_radius=10,
-                border=ft.border.all(1, ft.colors.GREY_900)
-            )
-        ]
-        # Atualiza a página para refletir as mudanças
-        page.update()    
-    
-    #Initialize selected_varas_list
-    selected_varas_list = ft.ResponsiveRow(
-        controls=[
-            ft.Container(
-                content=ft.ResponsiveRow(
-                    controls=[
-                        ft.Container(
-                            content=ft.Text(varas_labels.get(varas, varas), size=10),
-                            padding=ft.Padding(left=20, top=2, right=0, bottom=0),
-                            width=get_text_width(varas_labels.get(varas, varas), 10) + 40,
-                            height=25,
-                            bgcolor=ft.colors.BLUE,
-                            border_radius=5,
-                            #on_click=lambda e, v=varas: remove_varas(v),
-                            tooltip="Remover",
-                        ),
-                    ],
-                    spacing=5,
-                    alignment=ft.MainAxisAlignment.START,
-                ),
-                padding=5,
-            )
-            for varas in varas_selecionadas
-        ],
-        spacing=5,
-        run_spacing=10,
-    )
+        return      
 
     spinner_label = ft.Text("", size=10, color=ft.colors.BLUE_500)
     page.update()
 
 
-    # Adiciona todos os elementos em uma única chamada
     page.add(
         ft.Container(
             padding=ft.Padding(50, 50, 50, 50),  # Padding de 50 pixels em todos os lados
@@ -648,7 +516,6 @@ def main(pg: ft.Page):
                         padding=ft.Padding(0, 0, 0, 0)
                     ),
                     ft.Container(
-                        content=selected_varas_list,
                         padding=ft.Padding(0, 0, 0, 0)
                     ),
                     ft.Container(
@@ -681,7 +548,6 @@ def main(pg: ft.Page):
     )
 
     page.update()
-    update_varas_selecionadas()
     atualizar_rodape()  # Atualiza a nota de rodapé
 
 if __name__ == "__main__":
